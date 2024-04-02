f95a031054fbad8f1eaedf0cd0a8f4e6e3d88b5a
Louis Pallett <louis.pallett@outlook.com>
2024-04-02 23:53:31 +0100

    Created models

---
+++ models/country.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CountrySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 50,
+    },
+    abbreviation: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 3,
+    },
+    image_url: { type: String }
+});
+
+CountrySchema.virtual("url").get(function() {
+    return `/catalog/country/${this._id}`;
+});
+
+module.exports = mongoose.model("Country", CountrySchema);
---
+++ models/item.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 50,
+    },
+    description: { type: String, minLength: 10, required: true },
+    category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+    price: { type: Number, required: true },
+    stock_number: { type: Number, required: true },
+    country_of_origin: { type: Schema.Types.ObjectId, ref: "Country" },
+    strength: { type: Number }
+});
+
+ItemSchema.virtual("url").get(function() {
+    return `/catalog/item/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
