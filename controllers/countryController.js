6c379965e8cac5091b7e9385bc5b3e5f69447aef
Louis Pallett <louis.pallett@outlook.com>
2024-04-06 23:41:45 +0100

    Setup cloudinary configuration

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -5,6 +5,16 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const isUrlHttp = require("is-url-http");
 const isImageUrl = require('is-image-url');
+const cloudinary = require('cloudinary').v2;
+
+const [cloud_name, cloud_api_key, cloud_api_secret] = require("../public/javascripts/cloud_management");
+
+cloudinary.config({
+  cloud_name: cloud_name || process.env.CLOUDINARY_API_NAME,
+  api_key: cloud_api_key || process.env.CLOUDINARY_API_KEY,
+  api_secret: cloud_api_secret || process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 // Display list of all countries.
 exports.country_list = asyncHandler(async (req, res, next) => {
@@ -52,6 +62,28 @@ exports.country_create_post = [
     .escape(),
   body("image_url", "")
     .trim(),
+
+  /* 
+  Uploading an image file - the logic
+  
+  We've incorporated our cloudinary at the top of the page. When we display the flag on the page, we still fetch
+  this from the document in mongoDB, it's only that the URL in country.image_url is now a cloudinary URL (constant and 
+  managed by us) rather than an SVGRepo one which was external (and could change at any time with no input from us!)
+
+  So, fetching the flags is exactly the same - uploading them is a little different though - based on the above, when 
+  a file is uploaded and the user clicks submit
+    
+    First - check the image. We could check if it is an .svg image, for example.
+      If not
+        return error
+    Then, AWAIT upload of the image to Cloudinary
+      If error
+        return error and display it
+      Else
+        return the url from Cloudinary
+    Finally, set image_url to the returned value from Cloudinary
+    --> we can then carry on as normal, and set our new Country
+  */
   
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
